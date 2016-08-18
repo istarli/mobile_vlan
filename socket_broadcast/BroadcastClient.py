9108887075a72089df5d26763967cef1c0a2e145
star <i_starli@sina.com>
2016-08-18 19:38:47 +0800

    test

--- socket_broadcast/BroadcastClient.py
+++ socket_broadcast/BroadcastClient.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import socket,sys
 
-dst = ('192.168.124.255',12345)
+dst = ('10.0.0.255',12345)
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
 s.sendto("Hello",dst)
